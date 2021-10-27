9950f89630824efaa7fb4f46bd7499f2beaacdb8
bansony <bankovskaasona37@gmail.com>
2021-10-27 14:01:05 +0300

    Новая версия файла test.txt в ветке bankovsk

--- task/html/search/all_5.js
+++
@@ -1,9 +0,0 @@
-var searchData=
-[
-  ['save',['save',['../common_8h.html#a0dc9879063dd3bd3fdfd62ab8a01913d',1,'save(text txt, char *filename):&#160;save.c'],['../save_8c.html#a0dc9879063dd3bd3fdfd62ab8a01913d',1,'save(text txt, char *filename):&#160;save.c']]],
-  ['save_2ec',['save.c',['../save_8c.html',1,'']]],
-  ['show',['show',['../common_8h.html#a8c7ccfcd6288c249f9ccaee77c3928a9',1,'show(text txt):&#160;show.c'],['../show_8c.html#a8c7ccfcd6288c249f9ccaee77c3928a9',1,'show(text txt):&#160;show.c']]],
-  ['show_2ec',['show.c',['../show_8c.html',1,'']]],
-  ['shownum',['shownum',['../common_8h.html#afbcb98ae2eca828740dc897d14e5c32c',1,'shownum(text txt):&#160;shownum.c'],['../shownum_8c.html#afbcb98ae2eca828740dc897d14e5c32c',1,'shownum(text txt):&#160;shownum.c']]],
-  ['shownum_2ec',['shownum.c',['../shownum_8c.html',1,'']]]
-];
