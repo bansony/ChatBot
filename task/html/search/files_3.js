9a36cdecf969c360e95bb7f8f98a15a61d7748e1
bansony <bankovskaasona37@gmail.com>
2021-11-10 13:55:52 +0300

    Revert "Обновляем текст в тестовом файле"
    This reverts commit 8a61fe24f76a5f16cf0a18e011cc5e2eca6e4c0f.

--- task/html/search/files_3.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['m_2ec',['m.c',['../m_8c.html',1,'']]],
-  ['mcf_2ec',['mcf.c',['../mcf_8c.html',1,'']]]
-];
--- task/html/search/files_5.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['save_2ec',['save.c',['../save_8c.html',1,'']]],
-  ['show_2ec',['show.c',['../show_8c.html',1,'']]],
-  ['shownum_2ec',['shownum.c',['../shownum_8c.html',1,'']]]
-];
